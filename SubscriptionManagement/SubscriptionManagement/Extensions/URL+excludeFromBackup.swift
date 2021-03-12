61cf89879cdc03a23d3ac8d14becd57745069dc4
GOOKJO <mr.rnrnrn@gmail.com>
2021-03-13 02:47:17 +0900

    Cache Directory 백업 대상에서 제외하는 코드 구현

---
+++ SubscriptionManagement/SubscriptionManagement/Extensions/URL+excludeFromBackup.swift
@@ -0,0 +1,23 @@
+//
+//  URL+.swift
+//  SubscriptionManagement
+//
+//  Created by LoganBerry on 2021/03/13.
+//
+
+import Foundation
+
+extension URL {
+    mutating func excludeFromBackup() {
+        do {
+            let exist = try checkResourceIsReachable()
+            if exist {
+                var resourceValues = URLResourceValues()
+                resourceValues.isExcludedFromBackup = true
+                try setResourceValues(resourceValues)
+            }
+        } catch {
+            print(error)
+        }
+    }
+}
