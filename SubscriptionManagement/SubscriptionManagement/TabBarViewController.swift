700a64f58ddc82a5eb4c6876f1ecc616dfca3f4c
GOOKJO <mr.rnrnrn@gmail.com>
2021-04-20 01:51:23 +0900

    탭바컨트롤러에 진동 추가

--- SubscriptionManagement/SubscriptionManagement/TabBarViewController.swift
+++ SubscriptionManagement/SubscriptionManagement/TabBarViewController.swift
@@ -11,12 +11,12 @@ import AudioToolbox
 
 class TabBarViewController: UITabBarController {
 
+    
     override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
          
         // MARK:- weak vibration
-        //UIDevice.current.batteryLevel로 배터리 상태를 체크한 다음 사용
-        // AudioServicesPlaySystemSound(1519)
-        
+         AudioServicesPlaySystemSound(1519)
+    
     }
 
 }
