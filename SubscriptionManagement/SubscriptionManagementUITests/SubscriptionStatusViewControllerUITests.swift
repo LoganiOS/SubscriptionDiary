a2e4e56d9f59e332a599f9268d58668483ea0fac
GOOKJO <mr.rnrnrn@gmail.com>
2021-04-20 01:49:29 +0900

    [수정] 뷰에 접근할 때 accessibilityIdentifier로 접근하도록 변경

--- SubscriptionManagement/SubscriptionManagementUITests/SubscriptionStatusViewControllerUITests.swift
+++ SubscriptionManagement/SubscriptionManagementUITests/SubscriptionStatusViewControllerUITests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import SubscriptionManagement
 
 class SubscriptionStatusViewControllerUITests: XCTestCase {
 
@@ -22,50 +23,47 @@ class SubscriptionStatusViewControllerUITests: XCTestCase {
         app = nil
     }
     
-    private func given_whenTappedSortButton() {
-        app.buttons["SortButton"].tap()
+    private func identifier(_ matchedID: AccessibilityIdentifier) -> String {
+        return matchedID.rawValue
     }
     
-    private func given_whenTappedAddButton() {
-        app.buttons["Add"].tap()
+    private func given_whenTappedSortButton() {
+        app.buttons[identifier(.sortButton)].tap()
+    }
+    
+    private func given_whenTappedPlusButton() {
+        app.buttons[identifier(.plusButton)].tap()
     }
     
     private func given_whenTappedSortByNameActionSheetButton(sortBy buttonTitle: String) {
-        app.buttons["SortButton"].tap()
+        given_whenTappedSortButton()
         app.sheets.firstMatch.buttons["\(buttonTitle)"].tap()
     }
 
-    func testSubscriptionStatusViewController_whenTappedSortButton_didPresentActionSheet() {
+    func testSortButton_whenTapped_didPresentActionSheet() {
         given_whenTappedSortButton()
 
         XCTAssertTrue(app.sheets.firstMatch.exists)
     }
     
-    func testSubscriptionStatusViewController_whenTappedSortByNameActionSheetButton_didDisMissFromViewController() {
+    func testActionSheet_whenTappedSortByNameButton_didDisMissFromViewController() {
         given_whenTappedSortByNameActionSheetButton(sortBy: "이름순")
         
         XCTAssertFalse(app.sheets.firstMatch.exists)
     }
         
-    func testSubscriptionStatusViewController_whenTappedSortByPaymentActionSheetButton_didDisMissFromViewController() {
+    func testActionSheet_whenTappedSortByPaymentButton_didDisMissFromViewController() {
         given_whenTappedSortByNameActionSheetButton(sortBy: "결제금액순")
         
         XCTAssertFalse(app.sheets.firstMatch.exists)
     }
     
-    func testSubscriptionStatusViewController_whenTappedSortByPaymentDateActionSheetButton_didDisMissFromViewController() {
+    func testActionSheet_whenTappedSortByPaymentDateButton_didDisMissFromViewController() {
         given_whenTappedSortByNameActionSheetButton(sortBy: "결제일순")
         
         XCTAssertFalse(app.sheets.firstMatch.exists)
     }
-
-    func testSubscriptionStatusViewController_whenTappedAddButton_presentServiceListTableViewController() {
-        given_whenTappedAddButton()
-        
-        XCTAssertTrue(app.navigationBars["서비스 목록"].exists) 
-    }
-
-
     
+
     
 }
