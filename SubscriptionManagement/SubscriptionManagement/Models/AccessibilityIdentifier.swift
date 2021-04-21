9d274f9fe102898c6ab0ce5bcc9a12055f414a6e
GOOKJO <mr.rnrnrn@gmail.com>
2021-04-21 13:27:44 +0900

    accessibilityIdentifer case 추가

--- SubscriptionManagement/SubscriptionManagement/Models/AccessibilityIdentifier.swift
+++ SubscriptionManagement/SubscriptionManagement/Models/AccessibilityIdentifier.swift
@@ -7,9 +7,32 @@
 
 import Foundation
 
+
+/**
+ 속성의 accessibilityIdentifer를 지정할 때 이 case를 사용하세요.
+ */
 enum AccessibilityIdentifier: String {
     case sortButton
     case plusButton
     case leftBarButton
+    case rightBarButton
     case serviceListTableViewNavigationBar
+    case addServiceTableView
+    case nameField
+    case categorySeg
+    case payField
+    case startDateButton
+    case renewalDateButton
+    case pushSwith
+    case saveButton
+    case selectStartDateButton
+    case selectRenewalDateButton
+    case backButton
+    case deleteServiceButton
+    case favoriteTableView
+    case calendarView
+    case changeButton
+    case iconCollectionView
+    case colorCollectionView
+    case checkImageView
 }
